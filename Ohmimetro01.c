3c3092cf894e41c5023cded67fb8ffa3a16252e8
Davi Oliveira <davioliveira541@outlook.com>
2025-04-27 16:06:25 -0300

    update

--- Ohmimetro01.c
+++ Ohmimetro01.c
@@ -22,23 +22,32 @@
 #define OUT_PIN 7
 #define NUM_PIXELS 25
 
-int R_conhecido = 10000;    // Resistor de 10k ohm
+#define DEBOUNCE_TIME_MS 300 // Tempo de debounce em ms
+absolute_time_t last_interrupt_time = {0};
+
+int R_conhecido = 10000;   // Resistor de 10k ohm
 float R_x = 0.0;           // Resistor desconhecido
 float ADC_VREF = 3.31;     // Tensão de referência do ADC
 int ADC_RESOLUTION = 4095; // Resolução do ADC (12 bits)
 
 // Valores comerciais padrão E24 (5%)
-const int E24_VALUES[] = {510, 560, 620, 680, 750, 820, 910, 1000, 1100, 1200, 1300, 1500, 1600, 1800, 2000, 2200, 2400, 2700, 3000, 3300, 3600, 3900, 4300, 4700, 5100, 5600, 6200, 6800, 7500, 8200, 9100, 10000, 11000, 12000, 13000, 15000, 16000, 18000, 20000, 22000, 24000, 27000, 30000, 33000, 36000, 39000, 43000, 47000, 51000, 56000, 62000, 68000, 75000, 82000, 91000, 100000};
-
+const int E24_VALUES[] = {
+    510, 560, 620, 680, 750, 820, 910,
+    1000, 1100, 1200, 1300, 1500, 1600, 1800, 2000, 2200, 2400, 2700, 3000, 3300, 3600, 3900, 4300, 4700,
+    5100, 5600, 6200, 6800, 7500, 8200, 9100,
+    10000, 11000, 12000, 13000, 15000, 16000, 18000, 20000, 22000, 24000, 27000, 30000, 33000, 36000, 39000, 43000, 47000,
+    51000, 56000, 62000, 68000, 75000, 82000, 91000, 100000};
+bool is_four_band_mode = true; // Variável para controlar o número de faixas
 // Cores correspondentes às faixas do resistor
 const char *COLOR_CODES[] = {"Preto", "Marrom", "Vermelho", "Laranja", "Amarelo", "Verde", "Azul", "Violeta", "Cinza", "Branco"};
 
 uint32_t matrix_rgb(double r, double g, double b);
-void pio_drawn(double *desenho, uint32_t valor_led, PIO pio, uint sm, double r1, double g1, double b1, double r2, double g2, double b2, double r3, double g3, double b3);
+void pio_drawn(double desenho[][3], uint32_t valor_led, PIO pio, uint sm);
 int find_nearest_e24(float resistance);
-void get_color_bands(int resistance, int *band1, int *band2, int *multiplier);
-void get_color_bands(int resistance, int *band1, int *band2, int *multiplier);
+void get_color_bands(int resistance, int *band1, int *band2, int *band3, int *multiplier);
 void draw_resistor_bands(int band1, int band2, int multiplier, PIO pio, uint sm);
+void draw_resistor_bands_5(int band1, int band2, int band3, int multiplier, PIO pio, uint sm);
+void corrigir_linhas(double desenho[][3]);
 void gpio_irq_handler(uint gpio, uint32_t events);
 
 int main()
@@ -49,6 +58,11 @@ int main()
     gpio_set_dir(botaoB, GPIO_IN);
     gpio_pull_up(botaoB);
     gpio_set_irq_enabled_with_callback(botaoB, GPIO_IRQ_EDGE_FALL, true, &gpio_irq_handler);
+    gpio_init(Botao_A);
+    gpio_set_dir(Botao_A, GPIO_IN);
+    gpio_pull_up(Botao_A);
+    gpio_set_irq_enabled_with_callback(Botao_A, GPIO_IRQ_EDGE_FALL, true, &gpio_irq_handler);
+
     // Aqui termina o trecho para modo BOOTSEL com botão B
     PIO pio = pio0;
     uint32_t valor_led;
@@ -57,10 +71,6 @@ int main()
     uint offset = pio_add_program(pio, &pio_matrix_program);
     uint sm = pio_claim_unused_sm(pio, true);
     pio_matrix_program_init(pio, sm, offset, OUT_PIN);
-
-    gpio_init(Botao_A);
-    gpio_set_dir(Botao_A, GPIO_IN);
-    gpio_pull_up(Botao_A);
 
     i2c_init(I2C_PORT, 400 * 1000);
     gpio_set_function(I2C_SDA, GPIO_FUNC_I2C);
@@ -78,11 +88,10 @@ int main()
     adc_gpio_init(ADC_PIN);
 
     char str_resistance[10];
-    char str_band1[10], str_band2[10], str_multiplier[10];
-    //int index = 0;
+    char str_band1[10], str_band2[10], str_band3[10], str_multiplier[10];
+    char title[20];
     while (true)
     {
-        //R_conhecido = E24_VALUES[index];
         adc_select_input(2);
 
         float soma = 0.0f;
@@ -94,41 +103,50 @@ int main()
         float media = soma / 500.0f;
         R_x = (R_conhecido * media) / (ADC_RESOLUTION - media);
         int nearest_resistance = find_nearest_e24(R_x);
+        printf("R_x: %.2f\n", R_x);
 
-        int band1, band2, multiplier;
-        get_color_bands(nearest_resistance, &band1, &band2, &multiplier);
+        int band1, band2, band3, multiplier;
+        get_color_bands(nearest_resistance, &band1, &band2, &band3, &multiplier);
 
         sprintf(str_band1, "%s", COLOR_CODES[band1]);
         sprintf(str_band2, "%s", COLOR_CODES[band2]);
+        sprintf(str_band3, "%s", COLOR_CODES[band3]);
         sprintf(str_multiplier, "%s", COLOR_CODES[multiplier]);
         sprintf(str_resistance, "%d", nearest_resistance);
-        sprintf(str_band1, "%s", COLOR_CODES[band1]);
-        sprintf(str_band2, "%s", COLOR_CODES[band2]);
-        sprintf(str_multiplier, "%s", COLOR_CODES[multiplier]);
+        sprintf(title, "Ohmimetro %s", is_four_band_mode ? "4F" : "5F");
 
         int cor = 1;
-        // cor = !cor;
-        // Atualiza o conteúdo do display com animações
         ssd1306_fill(&ssd, !cor);                     // Limpa o display
         ssd1306_rect(&ssd, 3, 3, 122, 60, cor, !cor); // Desenha um retângulo
         ssd1306_line(&ssd, 3, 16, 123, 16, cor);      // Desenha uma linha
         ssd1306_line(&ssd, 3, 27, 123, 27, cor);
         ssd1306_line(&ssd, 3, 38, 123, 38, cor);
         ssd1306_line(&ssd, 3, 49, 123, 49, cor);
-        ssd1306_draw_string(&ssd, "Ohmimetro", 30, 6);     // Exibe "Ohmimetro"
-        ssd1306_draw_string(&ssd, "Fx1:", 6, 18);          // Exibe "Faixa 1:"
-        ssd1306_draw_string(&ssd, str_band1, 55, 18);      // Exibe o texto da primeira faixa de cor
-        ssd1306_draw_string(&ssd, "Fx2:", 6, 29);          // Exibe "Faixa 2:"
-        ssd1306_draw_string(&ssd, str_band2, 55, 29);      // Exibe o texto da segunda faixa de cor
+
+        ssd1306_draw_string(&ssd, title, 17, 6);      // Exibe "Ohmimetro"
+        ssd1306_draw_string(&ssd, "Fx1:", 6, 18);     // Exibe "Faixa 1:"
+        ssd1306_draw_string(&ssd, str_band1, 55, 18); // Exibe o texto da primeira faixa de cor
+        if (is_four_band_mode)
+        {
+            ssd1306_draw_string(&ssd, "Fx2:", 6, 29);     // Exibe "Faixa 2:"
+            ssd1306_draw_string(&ssd, str_band2, 55, 29); // Exibe o texto da segunda faixa de cor
+            draw_resistor_bands(band1, band2, multiplier, pio, sm);
+        }
+        else
+        {
+            ssd1306_draw_string(&ssd, "Fx2:", 6, 29);     // Exibe "Faixa 2:"
+            ssd1306_draw_string(&ssd, str_band2, 45, 29); // Exibe o texto da segunda faixa de cor
+            ssd1306_draw_string(&ssd, "Fx3:", 50, 29);    // Exibe "Faixa 3:"
+            ssd1306_draw_string(&ssd, str_band3, 55, 29); // Exibe o texto da segunda faixa de cor
+            draw_resistor_bands_5(band1, band2, band3, multiplier, pio, sm);
+        }
+
         ssd1306_draw_string(&ssd, "Mult.:", 6, 40);        // Exibe "Multiplicador:"
         ssd1306_draw_string(&ssd, str_multiplier, 55, 40); // Exibe o texto do multiplicador
         ssd1306_draw_string(&ssd, "Resi.:", 6, 52);        // Exibe "Resistencia:"
         ssd1306_draw_string(&ssd, str_resistance, 55, 52); // Exibe o valor da resistência
         ssd1306_send_data(&ssd);                           // Atualiza o display
-
-        draw_resistor_bands(band1, band2, multiplier, pio, sm);
-        sleep_ms(3000);
-        //index++;
+        sleep_ms(1000);
     }
 }
 
@@ -143,47 +161,17 @@ uint32_t matrix_rgb(double r, double g, double b)
 }
 
 // rotina para acionar a matrix de leds
-void pio_drawn(double *desenho, uint32_t valor_led, PIO pio, uint sm,
-               double r1, double g1, double b1, // Cor da coluna 1
-               double r2, double g2, double b2, // Cor da coluna 2
-               double r3, double g3, double b3) // Cor da coluna 3
+void pio_drawn(double desenho[][3], uint32_t valor_led, PIO pio, uint sm)
 {
     for (int16_t i = 0; i < NUM_PIXELS; i++)
     {
-        // Determina a linha e coluna
-        int linha = i / 5;
-        int coluna = i % 5;
+        
+        double r = desenho[i][0] * 0.1;
+        double g = desenho[i][1] * 0.1;
+        double b = desenho[i][2] * 0.1;
 
-        // Ajusta as colunas para linhas invertidas (1 e 4)
-        if (linha == 1 || linha == 3) // Linhas 1 e 4
-        {
-            if (coluna == 1)
-                coluna = 3; // Coluna 2 vira 4
-            else if (coluna == 2)
-                coluna = 2; // Coluna 3 permanece
-            else if (coluna == 3)
-                coluna = 1; // Coluna 4 vira 2
-        }
-
-        // Determina a cor com base na coluna
-        if (coluna == 3) // Coluna 2
-        {
-            valor_led = matrix_rgb(r1 * desenho[i], g1 * desenho[i], b1 * desenho[i]);
-        }
-        else if (coluna == 2) // Coluna 3
-        {
-            valor_led = matrix_rgb(r2 * desenho[i], g2 * desenho[i], b2 * desenho[i]);
-        }
-        else if (coluna == 1) // Coluna 4
-        {
-            valor_led = matrix_rgb(r3 * desenho[i], g3 * desenho[i], b3 * desenho[i]);
-        }
-        else
-        {
-            valor_led = 0; // Pixels fora das colunas centrais ficam apagados
-        }
-
-        // Envia o valor para a PIO
+        // Obtém o valor RGB e envia para a PIO
+        valor_led = matrix_rgb(r, g, b);
         pio_sm_put_blocking(pio, sm, valor_led);
     }
 }
@@ -203,7 +191,7 @@ int find_nearest_e24(float resistance)
 }
 
 // Função para determinar as cores das faixas
-void get_color_bands(int resistance, int *band1, int *band2, int *multiplier)
+void get_color_bands(int resistance, int *band1, int *band2, int *band3, int *multiplier)
 {
     // Garante que a resistência está dentro de um intervalo válido
     if (resistance <= 0)
@@ -215,32 +203,90 @@ void get_color_bands(int resistance, int *band1, int *band2, int *multiplier)
     int digits = resistance;
     int pow10 = 0;
 
-    // Reduz até que só restem dois dígitos significativos
-    while (digits >= 100)
+    // Reduz até que só restem três dígitos significativos no modo de 5 faixas
+    if (is_four_band_mode)
     {
-        digits /= 10;
-        pow10++;
+        while (digits >= 100)
+        {
+            digits /= 10;
+            pow10++;
+        }
+        *band1 = digits / 10; // Primeiro dígito
+        *band2 = digits % 10; // Segundo dígito
+        *multiplier = pow10;  // Potência de 10
     }
-
-    *band1 = digits / 10; // primeiro dígito
-    *band2 = digits % 10; // segundo dígito
-    *multiplier = pow10;  // potência de 10 (ex: 10^3 → laranja)
+    // else
+    // {
+    //     while (digits >= 100)
+    //     {
+    //         digits /= 10;
+    //         pow10++;
+    //     }
+    //     *band1 = digits / 10; // Primeiro dígito
+    //     *band2 = digits % 10; // Segundo dígito
+    //     *multiplier = pow10;  // Potência de 10 (multiplicador)
+    // }
+    else
+    {
+        while (digits >= 1000)
+        {
+            digits /= 10;
+            pow10++;
+        }
+        *band1 = digits / 100;       // Primeiro dígito
+        *band2 = (digits / 10) % 10; // Segundo dígito
+        *band3 = digits % 10;        // Terceiro dígito
+        *multiplier = pow10;         // Potência de 10 (multiplicador)
+    }
 }
 
 // Trecho para modo BOOTSEL com botão B
 void gpio_irq_handler(uint gpio, uint32_t events)
 {
-    reset_usb_boot(0, 0);
-}
+    absolute_time_t current_time = get_absolute_time();
 
+    if (absolute_time_diff_us(last_interrupt_time, current_time) < DEBOUNCE_TIME_MS * 1000)
+    {
+        return; // Ignora a interrupção se estiver dentro do tempo de debounce
+    }
+    else
+    {
+        last_interrupt_time = current_time;
+    }
+
+    // Ativa ou desativa a funcionalidade do LED com PWM quando o botão A é pressionado
+    if (gpio == Botao_A)
+    {
+        is_four_band_mode = !is_four_band_mode; // Alterna entre os modos de 4 e 5 faixas
+    }
+    if (gpio == botaoB)
+    {
+        reset_usb_boot(0, 0);
+    }
+}
+void corrigir_linhas(double desenho[][3])
+{
+    // Troca os valores da linha 1 com a linha 4
+    for (int i = 0; i < 25; i++)
+    {
+        printf("[%.1f]", desenho[i]); // Imprime o elemento
+
+        if ((i + 1) % 5 == 0) // Adiciona uma nova linha após 5 elementos
+        {
+            printf("\n");
+        }
+    }
+
+    
+}
 void draw_resistor_bands(int band1, int band2, int multiplier, PIO pio, uint sm)
 {
     // Mapeia as cores para valores RGB
     double color_map[10][3] = {
-        {0.001, 0.001, 0.001},  // Preto
-        {1, 0.125, 0.0}, // Marrom
+        {0, 0, 0},        // Preto
+        {1, 0.125, 0.0},  // Marrom
         {1.0, 0.0, 0.0},  // Vermelho
-        {1.0, 0.25, 0.0},  // Laranja
+        {1.0, 0.25, 0.0}, // Laranja
         {1.0, 1.0, 0.0},  // Amarelo
         {0.0, 1.0, 0.0},  // Verde
         {0.0, 0.0, 1.0},  // Azul
@@ -250,18 +296,113 @@ void draw_resistor_bands(int band1, int band2, int multiplier, PIO pio, uint sm)
     };
 
     // Matriz 5x5 representada como um array de 25 pixels
-    double desenho[NUM_PIXELS] = {0};
+    double desenho[NUM_PIXELS][3] = {0};
 
     // Define as colunas centrais para as faixas e o multiplicador
     for (int i = 0; i < 5; i++)
     {
-        desenho[i * 5 + 1] = 0.1; // Coluna 2 para a primeira faixa
-        desenho[i * 5 + 2] = 0.1; // Coluna 3 para a segunda faixa
-        desenho[i * 5 + 3] = 0.1; // Coluna 4 para o multiplicador
+        if (i % 2 == 0)
+        {
+
+            desenho[i * 5 + 3][0] = color_map[band1][0]; // R
+            desenho[i * 5 + 3][1] = color_map[band1][1]; // G
+            desenho[i * 5 + 3][2] = color_map[band1][2]; // B
+            
+            desenho[i * 5 + 2][0] = color_map[band2][0]; // R
+            desenho[i * 5 + 2][1] = color_map[band2][1]; // G
+            desenho[i * 5 + 2][2] = color_map[band2][2]; // B
+
+            desenho[i * 5 + 1][0] = color_map[multiplier][0]; // R
+            desenho[i * 5 + 1][1] = color_map[multiplier][1]; // G
+            desenho[i * 5 + 1][2] = color_map[multiplier][2]; // B
+        }
+        else
+        {
+            desenho[i * 5 + 1][0] = color_map[band1][0]; // R
+            desenho[i * 5 + 1][1] = color_map[band1][1]; // G
+            desenho[i * 5 + 1][2] = color_map[band1][2]; // B
+    
+            // Faixa 2 (coluna 3)
+            desenho[i * 5 + 2][0] = color_map[band2][0]; // R
+            desenho[i * 5 + 2][1] = color_map[band2][1]; // G
+            desenho[i * 5 + 2][2] = color_map[band2][2]; // B
+
+            // Multiplicador (coluna 4)
+            desenho[i * 5 + 3][0] = color_map[multiplier][0]; // R
+            desenho[i * 5 + 3][1] = color_map[multiplier][1]; // G
+            desenho[i * 5 + 3][2] = color_map[multiplier][2]; // B
+        }
     }
 
-    pio_drawn(desenho, 0, pio, sm,
-              color_map[band1][0], color_map[band1][1], color_map[band1][2],                 // Cor da coluna 2
-              color_map[band2][0], color_map[band2][1], color_map[band2][2],                 // Cor da coluna 3
-              color_map[multiplier][0], color_map[multiplier][1], color_map[multiplier][2]); // Cor da coluna 4
+    corrigir_linhas(desenho);
+
+    pio_drawn(desenho, 0, pio, sm); // Cor da coluna 4
+}
+
+void draw_resistor_bands_5(int band1, int band2, int band3, int multiplier, PIO pio, uint sm)
+{
+    // Mapeia as cores para valores RGB
+    double color_map[10][3] = {
+        {0, 0, 0},        // Preto
+        {1, 0.125, 0.0},  // Marrom
+        {1.0, 0.0, 0.0},  // Vermelho
+        {1.0, 0.25, 0.0}, // Laranja
+        {1.0, 1.0, 0.0},  // Amarelo
+        {0.0, 1.0, 0.0},  // Verde
+        {0.0, 0.0, 1.0},  // Azul
+        {0.5, 0.0, 0.5},  // Violeta
+        {0.5, 0.5, 0.5},  // Cinza
+        {1.0, 1.0, 1.0}   // Branco
+    };
+
+    // Matriz 5x5 representada como um array de 25 pixels
+    double desenho[NUM_PIXELS][3] = {0};
+
+    // Define as colunas centrais para as faixas e o multiplicador
+    for (int i = 0; i < 5; i++)
+    {
+        if (i % 2 == 0)
+        {
+
+            desenho[i * 5 + 3][0] = color_map[band1][0]; // R
+            desenho[i * 5 + 3][1] = color_map[band1][1]; // G
+            desenho[i * 5 + 3][2] = color_map[band1][2]; // B
+            
+            desenho[i * 5 + 2][0] = color_map[band2][0]; // R
+            desenho[i * 5 + 2][1] = color_map[band2][1]; // G
+            desenho[i * 5 + 2][2] = color_map[band2][2]; // B
+            
+            desenho[i * 5 + 1][0] = color_map[band3][0]; // R
+            desenho[i * 5 + 1][1] = color_map[band3][1]; // G
+            desenho[i * 5 + 1][2] = color_map[band3][2]; // B
+            
+            desenho[i * 5 + 0][0] = color_map[multiplier][0]; // R
+            desenho[i * 5 + 0][1] = color_map[multiplier][1]; // G
+            desenho[i * 5 + 0][2] = color_map[multiplier][2]; // B
+        }
+        else
+        {
+            desenho[i * 5 + 1][0] = color_map[band1][0]; // R
+            desenho[i * 5 + 1][1] = color_map[band1][1]; // G
+            desenho[i * 5 + 1][2] = color_map[band1][2]; // B
+    
+            // Faixa 2 (coluna 3)
+            desenho[i * 5 + 2][0] = color_map[band2][0]; // R
+            desenho[i * 5 + 2][1] = color_map[band2][1]; // G
+            desenho[i * 5 + 2][2] = color_map[band2][2]; // B
+    
+            desenho[i * 5 + 3][0] = color_map[band3][0]; // R
+            desenho[i * 5 + 3][1] = color_map[band3][1]; // G
+            desenho[i * 5 + 3][2] = color_map[band3][2]; // B
+    
+            // Multiplicador (coluna 4)
+            desenho[i * 5 + 4][0] = color_map[multiplier][0]; // R
+            desenho[i * 5 + 4][1] = color_map[multiplier][1]; // G
+            desenho[i * 5 + 4][2] = color_map[multiplier][2]; // B
+        }
+    }
+    corrigir_linhas(desenho);
+
+    // Chama a função pio_drawn para desenhar as faixas
+    pio_drawn(desenho, 0, pio, sm);
 }
